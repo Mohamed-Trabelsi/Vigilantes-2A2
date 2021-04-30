d8c5fe529b60d62401f8090f032733b492dfa455
zribimoenes <moenes.zribi@esprit.tn>
2021-04-30 14:35:48 +0100

    modification CRUD hotel

--- view/showAlbums.php
+++ view/showAlbums.php
@@ -17,39 +17,72 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
 </head>
 
 <body>
 	
 	<a href = "searchAlbum.php" class="btn btn-primary shop-item-button">Search</a>
-		<section class="container">
+	
 			<h2>HOTEL</h2>
-			<a href = "addAlbum.php" class="btn btn-primary shop-item-button" href = "#">Ajouter</a>
-			<div class="shop-items">
+			  
+
+			  <div class="panel"><div class="table-responsive"><table class="table table-striped title1">
+						<tr><td><b>S.N.</b></td><td><b>nom</b></td><td><b>Adresse</b></td><td><b>chambres disponibles</b></td><td><b>prix</b></td><td><b>By</b></td>
+
+						<b><td><a href = "addAlbum.php" class="btn btn-primary shop-item-button" href = "#">Ajouter</a></td>
+						   <td></td></tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
+		
+			
 				<?php
 					foreach ($albums as $hotel) {
 				?>
-				<div class="shop-item">
-					<strong class="shop-item-title"> <?= $hotel['name'] ?> </strong>
-					<span class="shop-item-price"><?= $hotel['prix'] ?> dt.</span>
-					<span class="shop-item-adresse"><?= $hotel['adresse'] ?></span>
-					<span class="shop-item-caracteristiques"><?= $hotel['caracteristiques'] ?></span>
-					<span class="shop-item-chambre"><?= $hotel['chambre'] ?></span>
-					<img src="../images/<?= $hotel['image'] ?>" width = "200" height = "200" class="shop-item-image">
-
-					<div class="shop-item-details">
+				
+<tr>
+	<td><strong class="shop-item-title"> <?= $hotel['name'] ?> </strong></td>
+	<td><span class="shop-item-price"><?= $hotel['prix'] ?> dt.</span></td>
+	<td><span class="shop-item-adresse"><?= $hotel['adresse'] ?></span></td>
+	<td><span class="shop-item-caracteristiques"><?= $hotel['caracteristiques'] ?></span></td>
+	<td><span class="shop-item-chambre"><?= $hotel['chambre'] ?></span></td>
+	<td><img src="../images/<?= $hotel['image'] ?>" width = "200" height = "200" class="shop-item-image">
+</td>
+	<td>
+						<a type="button" class="btn btn-primary shop-item-button" href = "updateAlbum.php?id=<?= $hotel['id'] ?>">Modifier</a></td>
+	<td><a type="button" class="btn btn-primary shop-item-button" href = "showAlbums.php?id=<?= $hotel['id'] ?>">Supprimer</a></td>
+</tr>
+					
+					
+					
+				
+					
+					
+					
 						
-						<a type="button" class="btn btn-primary shop-item-button" href = "updateAlbum.php?id=<?= $hotel['id'] ?>">Modifier</a>
-						<a type="button" class="btn btn-primary shop-item-button" href = "showAlbums.php?id=<?= $hotel['id'] ?>">Supprimer</a>
-					</div>
-				</div>
+						
+					
+				
 				<?php 
 					}
 				?>
-			</div>
-		</section>
+		
+	
 
 	
 
